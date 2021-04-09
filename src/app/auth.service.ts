4c4a2c25748064f4c2baca3dc5d41cdefd80ed56
Filipe Alves <filipemouraalves@gmail.com>
2021-04-09 06:40:50 -0300

    Melhorias na tela de login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Usuario } from './login/usuario';
@@ -13,13 +14,40 @@ export class AuthService {
   tokenURL: string = environment.apiURLBase + environment.obterTokenUrl;
   clientId: string = environment.clientId;
   clientSecret: string = environment.clientSecret;
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor(
     private http: HttpClient
   ) {}
 
+  obterToken() {
+    const tokenString = localStorage.getItem('access_token');
+
+    if(tokenString) {
+      return JSON.parse(tokenString).access_token;
+    }
+
+    return null;
+  }
+
+  encerrarSessao() {
+    localStorage.removeItem('access_token');
+  }
+
+  getUsuarioAutenticado() {
+    const token = this.obterToken();
+
+    return token ? this.jwtHelper.decodeToken(token).user_name : null;
+  }
+
   isAuthenticated() : boolean {
-    return true;
+    const token = this.obterToken();
+
+    if(token) {
+      return !this.jwtHelper.isTokenExpired(token);
+    }
+
+    return false;
   }
 
   salvar(usuario: Usuario) : Observable<any> {
